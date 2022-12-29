db877f9ae0e5717ef15fb5b5687ac702613e9efe
Julián Andrés Rodríguez Vásquez <116516937+JulianRodriguez08@users.noreply.github.com>
2022-12-29 16:33:54 -0500

    Add files via upload

---
+++ Javascript Básico/Ejercicio 2/Ejercicio 2.js
@@ -0,0 +1,12 @@
+const FechaNacimiento = new Date(1994, 9, 17);
+
+const LibroFavorito = {
+    titulo: "Nuestro planeta, nuestro futuro",
+    autor: "Manuel Rodríguez Becerra",
+    fecha: "2019",
+    url: "https://www.buscalibre.com.co/libro-nuestro-planeta-nuestro-futuro/9789585446748/p/51942373",
+}
+
+const Ejercicio2 = ["Julián Rodríguez", 28, true, FechaNacimiento, LibroFavorito];
+
+console.log(Ejercicio2);
